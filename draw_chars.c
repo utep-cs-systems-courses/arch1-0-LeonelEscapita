13f8e55eb1ffc66c7ccc6213be160592d7d38681
student <student@systems-vm.lan>
2021-02-16 00:01:50 -0700

    changed orientation on font

--- draw_chars.c
+++ draw_chars.c
@@ -17,9 +17,9 @@ void print_char_11x16(char c)
 void print_char_5x7(char c)
 {
   c-=0x20;
-  for (char row = 0; row < 5; row++) {
-    unsigned char rowBits = font_5x7[c][row];
-    for (char col = 0; col < 7; col++) {
+  for (char col = 6; col >= 0; col--) {
+    for (char row = 0; row < 5; row++) {
+      unsigned char rowBits = font_5x7[c][row];
       unsigned char colMask = 1 << (6-col);
       putchar( (rowBits & colMask) ? '*' : ' ');
     }
